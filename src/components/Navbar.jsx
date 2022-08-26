650c40b1e60e4f6a51c36c31d5d71beba9606e63
Sashank Rampalli <sashankr97@gmail.com>
2022-08-27 01:54:15 +0530

    Added mobile sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,36 @@
 import "../styles/Navbar.css";
 import LogoIcon from "../assets/logo.svg";
 import HamburgerIcon from "../assets/icons/hamburger.svg";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <header className="navbar-container">
-      <nav className="navbar">
-        <div className="navbar-toggler">
-          <img src={HamburgerIcon} alt="menu toggler for mobile" />
-        </div>
-        <img src={LogoIcon} alt="scoot logo" />
-      </nav>
-    </header>
+    <>
+      <header className="navbar-container">
+        <nav className="navbar">
+          <div className="navbar-toggler">
+            <img src={HamburgerIcon} alt="menu toggler for mobile" />
+          </div>
+          <img src={LogoIcon} alt="scoot logo" />
+        </nav>
+      </header>
+      <div className="navbar-mobile-menu">
+        <ul className="navbar-mobile-menu-list">
+          <li className="navbar-mobile-menu-list-item">
+            <Link to="/">About</Link>
+          </li>
+          <li className="navbar-mobile-menu-list-item">
+            <Link to="/">Location</Link>
+          </li>
+          <li className="navbar-mobile-menu-list-item">
+            <Link to="/">Careers</Link>
+          </li>
+        </ul>
+        <button className="navbar-mobile-menu-action-button">
+          Get Scootin
+        </button>
+      </div>
+    </>
   );
 };
 
