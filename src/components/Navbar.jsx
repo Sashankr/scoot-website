9cf7aa5ca1a693a57e604326ce1a29612b8e57e0
Sashank Rampalli <sashankr97@gmail.com>
2022-08-26 11:36:28 +0530

    updated toggler with image

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,13 @@
 import "../styles/Navbar.css";
 import LogoIcon from "../assets/logo.svg";
+import HamburgerIcon from "../assets/icons/hamburger.svg";
 
 const Navbar = () => {
   return (
     <header className="navbar-container">
       <nav className="navbar">
         <div className="navbar-toggler">
-          <span></span>
-          <span></span>
-          <span></span>
+          <img src={HamburgerIcon} alt="menu toggler for mobile" />
         </div>
         <img src={LogoIcon} alt="scoot logo" />
       </nav>
