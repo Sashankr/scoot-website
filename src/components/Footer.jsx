793116b6a97af51314a3662426cc7c96550627bb
Sashank Rampalli <sashankr97@gmail.com>
2022-09-11 15:43:50 +0530

    Completed footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/Footer.css";
 import LightLogo from "./LightLogo";
+import FacebookLogo from "../assets/icons/facebook.svg";
+import TwitterLogo from "../assets/icons/twitter.svg";
+import InstagramLogo from "../assets/icons/instagram.svg";
 
 const Footer = () => {
   return (
@@ -10,7 +13,7 @@ const Footer = () => {
         <div className="footer-logo">
           <LightLogo />
         </div>
-        <ul>
+        <ul className="footer-logo-list-container">
           <li className="footer-logo-list">
             <Link to="/">About</Link>
           </li>
@@ -21,6 +24,11 @@ const Footer = () => {
             <Link to="/">Careers</Link>
           </li>
         </ul>
+      </div>
+      <div className="footer-socials">
+        <img src={FacebookLogo} alt="facebook" />
+        <img src={TwitterLogo} alt="twitter" />
+        <img src={InstagramLogo} alt="instagram" />
       </div>
     </footer>
   );
